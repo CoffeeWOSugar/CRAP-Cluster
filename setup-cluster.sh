fd94edd8a5253fcaa788a6461558dfa2b448d984
Villiam sundström <villiam.sundstrom.2055@student.uu.se>
2025-11-24 13:48:42 +0100

    Added options -h help and -v verbose

--- setup-cluster.sh
+++ setup-cluster.sh
@@ -16,8 +16,44 @@ gitCommand="
 GIT_SSH_COMMAND='ssh -p 22222 -o HostName=localhost -o User=git' && git clone git@localhost:CoffeeWOSugar/\"$repo\".git"
 
 
+
+usage(){
+	echo "Usage: $0 "
+	echo
+	echo "Options:"
+	echo "	-h	Display help message"
+	echo "	-v	Verbose output"
+	exit 1
+}
+
+helptext(){
+	echo "This script installs all neccesary dependencies on specified nodes"
+	echo "The IP addresses to the nodes should be specified in the file"
+	echo "nodes.cnf on the following format:"
+	echo "	IP sudopassword"
+	echo "-------------------------------------------------------------------"
+	echo
+}
+
+while getopts ":hv" o; do
+    case "${o}" in
+        h)
+	    helptext
+	    usage
+            ;;
+        v)
+            verbose="-v"}
+            ;;
+        *)
+            usage
+            ;;
+    esac
+done
+
+
 fetchBuilds="if [ ! -d \"$repo\" ]; then $gitCommand && chmod +x \"$repo\"/*.sh ; fi && cd \"$repo\" && git pull"
-buildBuilds="cd \"$repo\" && echo "vincent" | sudo -S ./build.sh"
+buildBuilds="cd \"$repo\" && echo "vincent" | sudo -S ./build.sh "${verbose}""
+
 
 while IFS="" read -r node || [ -n "$node" ]
 do
