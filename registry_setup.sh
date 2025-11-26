e802c2bf6ef81c99ef4f35e1fa35d4bb503a66ef
Vincent <vinc.anders@gmail.com>
2025-11-26 12:57:31 +0100

    Updated requirements.txt to work with more modern installations and added instructions in registry_setup.sh.

--- registry_setup.sh
+++ registry_setup.sh
@@ -1,1 +1,5 @@
-docker service create --name registry --publish published=5000,target=5000 registry:2
+# Start registry service on manager node
+docker service create --name cluster-registry --publish published=5000,target=5000 --restart=always registry:2
+
+# Then push the stack/container to the registry using "docker push cluster-registry:5000/docker_stack_demo:1.0
+# And start the service on each machine using docker service create --name docker_stack_demo cluster-registry:5000/docker_stack_demo:1.0
