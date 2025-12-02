4c8d1879b266ffe1293d23e9e976125f87bc7355
villiam <ville@Bettan.localdomain>
2025-12-02 11:15:50 +0100

    File structure and dependencies for ui

---
+++ frontend/src/App.jsx
@@ -0,0 +1,8 @@
+export default function App() {
+  return (
+    <div style={{ padding: 30 }}>
+      <h1>Electron + Vite + React</h1>
+      <p>If you see this, everything works! 🚀</p>
+    </div>
+  );
+}
