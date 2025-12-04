9bbcaf29d05afbb0f908439a1ed8c00cd91ff9e2
elsareuter <elsa.reutersward@hotmail.com>
2025-12-04 11:13:45 +0100

    Added network create in registry setup

--- registry_setup.sh
+++ registry_setup.sh
@@ -1,5 +1,8 @@
 # Start registry service on manager node
-docker service create --name cluster-registry --publish published=5000,target=5000 --restart=always registry:2
+docker service create --name cluster-registry --publish published=5000,target=5000 --restart-condition=any registry:2
+
+docker network create -d overlay mpi-net
+
 
 # Then push the stack/container to the registry using "docker push cluster-registry:5000/docker_stack_demo:1.0
 # And start the service on each machine using docker service create --name docker_stack_demo cluster-registry:5000/docker_stack_demo:1.0
