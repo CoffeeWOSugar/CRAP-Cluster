e82a4523d7dcd5802fb2c6a8f85566eb3714054d
Gamlabettan <villiam.sundstrom.2055@student.uu.se>
2025-12-09 10:14:59 +0100

    fixed master ip adress -ebba-

--- swarm_setup.sh
+++ swarm_setup.sh
@@ -2,22 +2,21 @@
 
 # Check if docker is installed
 if [[ "$(which docker | wc -l)" -eq "0" ]]; then
-    echo "Docker installation is missing."
-    exit 0
+  echo "Docker installation is missing."
+  exit 0
 fi
 
 # Docker swarm init on manager
-sudo docker swarm leave --force 
-swarm_init_output=$(sudo docker swarm init --advertise-addr 192.168.10.3)
+sudo docker swarm leave --force
+swarm_init_output=$(sudo docker swarm init --advertise-addr 192.168.10.1)
 echo "$swarm_init_output"
 join_command=$(printf "%s\n" "$swarm_init_output" | grep "docker swarm join")
 
 #join_command=$(printf "%s\n" "$swarm_init_output" | awk '/To add a worker/{flag=1; next} flag && NF{printf "%s ", $0} NF==0 && flag{exit}')
 echo "$join_command"
 # Perform Join command on each node:
-while IFS="" read -r node || [ -n "$node" ]
-do
- 
+while IFS="" read -r node || [ -n "$node" ]; do
+
   arr=($node)
   name="${arr[0]}"
   pass="${arr[1]}"
@@ -25,8 +24,8 @@ do
   echo "$pass"
 
   echo ">>> connecting $node to swarm..."
-  sshpass -p "$pass" ssh -n -q -A "$name" "echo 'vincent' | sudo -S docker swarm leave --force" 
+  sshpass -p "$pass" ssh -n -q -A "$name" "echo 'vincent' | sudo -S docker swarm leave --force"
   sshpass -p "$pass" ssh -n -q -A "$name" "echo 'vincent' | sudo -S bash -c '$join_command'"
 
-done < nodes.cnf
+done <nodes.cnf
 sudo docker node ls
