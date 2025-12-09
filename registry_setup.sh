212bd44cab57585e0f52671e14df9eab582a4dc7
Gamlabettan <villiam.sundstrom.2055@student.uu.se>
2025-12-09 13:48:39 +0100

    Updated registry setup

--- registry_setup.sh
+++ registry_setup.sh
@@ -1,8 +1,11 @@
 # Start registry service on manager node
 docker service create --name cluster-registry --publish published=5000,target=5000 --restart-condition=any registry:2
+<<<<<<< HEAD
 
 docker network create -d overlay mpi-net
 
+=======
+>>>>>>> 595ae6e (Updated registry setup)
 
 # Then push the stack/container to the registry using "docker push cluster-registry:5000/docker_stack_demo:1.0
 # And start the service on each machine using docker service create --name docker_stack_demo cluster-registry:5000/docker_stack_demo:1.0
